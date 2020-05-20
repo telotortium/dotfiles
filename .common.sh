64311aab7400eeb0d2d8a861f91008c0887d1752
Robert Irelan <rirelan@gmail.com>
2020-05-20 14:22:39 -0700

    Speed up command_on_path

--- .common.sh
+++ .common.sh
@@ -6,13 +6,16 @@
 # Exit successfully if CMD is an executable (or a shell builtin) or
 # unsuccessfully otheriwse.
 command_on_path () {
-    # `command -v` is specified by POSIX (see
-    # http://pubs.opengroup.org/onlinepubs/009696699/utilities/command.html).
-    #
-    # Execute shell explicitly in order to avoid loading startup files (e.g.,
-    # .env, .bashrc, .kshrc).
-    #
-    # `$1` is intentionally evaluated in the subshell, not this shell.
-    # shellcheck disable=SC2016
-    /usr/bin/env sh -c 'command -v "$1" >/dev/null 2>&1' IGNORED "$1"
+    {
+        cmd=$1
+        # Fast path - use hash
+        hash "$cmd" || (
+            # Slow path, if hash fails or is unsupported.
+            set -e
+            unalias -a
+            x=$(command -v "$cmd")
+            # If command starts with `/`, it's not a builtin command.
+            test "${x#/}" != "${x}"
+        )
+    } >/dev/null 2>&1
 }
