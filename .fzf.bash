f7e7523c21976fbd31039a252ea3a0157e488a29
Robert Irelan <rirelan@gmail.com>
2022-02-09 13:03:45 -0800

    Add fzf shell configs

---
+++ .fzf.bash
@@ -0,0 +1,13 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */Users/bytedance/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/Users/bytedance/.fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/Users/bytedance/.fzf/shell/completion.bash" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/Users/bytedance/.fzf/shell/key-bindings.bash"
