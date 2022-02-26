1e3bfbed69bcb7e3b0ffa4d773dfb1991bfb67c9
Robert Irelan <rirelan@gmail.com>
2022-02-26 10:21:01 -0800

    Add profile for Cask Emacs

---
+++ .cask-emacs.d/init.el
@@ -0,0 +1,15 @@
+(require 'cask "/opt/local/share/emacs/site-lisp/cask.el")
+(cask-initialize)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(ansi shut-up epl git commander f dash s package-build))
+ '(warning-suppress-log-types '((comp))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
