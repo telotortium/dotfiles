b6138df475926320d6d8082d772e9366cc8b2fee
Robert Irelan <rirelan@gmail.com>
2023-10-04 19:46:29 -0700

    Generalize HOME location in fzf init files

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */Users/rmi1/.fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}/Users/rmi1/.fzf/bin"
+if [[ ! "$PATH" == *${HOME}/.fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}${HOME}/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/Users/rmi1/.fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "${HOME}/.fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/Users/rmi1/.fzf/shell/key-bindings.zsh"
+source "${HOME}/.fzf/shell/key-bindings.zsh"
