0a14f6a309a4bb177393b3606688874ecac10c2b
Robert Irelan <rirelan@gmail.com>
2026-03-10 14:35:11 -0700

    Fix idempotency bugs in .dotfiles-setup.bash
    The script had two idempotency bugs:
    
    - it tried `git checkout HEAD -- "$x"` even for paths that no longer exist in HEAD
    - it hard-failed forever once `~/.dotfiles-git-dir-backup` existed

--- .dotfiles-setup.bash
+++ .dotfiles-setup.bash
@@ -4,21 +4,27 @@ set -eu
 main () {
     local git_dir="$HOME/.dotfiles-git-dir"
     local git_dir_backup="${git_dir}-backup"
-    if [ -e "${git_dir_backup}" ]; then
-        echo "Backup directory ${git_dir_backup} exists -- " \
-            "move and try again" 1>&2
-        return 1;
-    fi
-    [ -e "${git_dir}" ] && mv "${git_dir}" "${git_dir_backup}"
-    local repo="https://github.com/telotortium/dotfiles"
-    local tmp_dir="$HOME/dotfiles-tmp"
     local worktree="$HOME"
-    git clone --separate-git-dir="${git_dir}"  "${repo}" "${tmp_dir}"
-    cp "${tmp_dir}/.gitmodules" "${worktree}"
-    rm -rf "${tmp_dir}"
     config() {
         git --git-dir="${git_dir}" "$@"
     }
+
+    if [ ! -e "${git_dir}" ] && [ -e "${git_dir_backup}" ]; then
+        mv "${git_dir_backup}" "${git_dir}"
+    fi
+    if [ -e "${git_dir}" ]; then
+        config config --local core.worktree "${worktree}"
+        config config --local status.showUntrackedFiles no
+        config submodule update --init --recursive
+        return 0
+    fi
+
+    [ -e "${git_dir_backup}" ] && rm -rf "${git_dir_backup}"
+    local repo="https://github.com/telotortium/dotfiles"
+    local tmp_dir="$HOME/dotfiles-tmp"
+    git clone --separate-git-dir="${git_dir}"  "${repo}" "${tmp_dir}"
+    cp "${tmp_dir}/.gitmodules" "${worktree}"
+    rm -rf "${tmp_dir}"
 
     # Set working tree
     config config --local core.worktree "${worktree}"
@@ -28,7 +34,11 @@ main () {
         config status --porcelain \
             | sed -ne '/^ / { s/^ *[^ ]* *\(.*\)$/\1/; p; }'
     }
-    existing_dotfiles | while read -r x; do config checkout HEAD -- "$x"; done
+    existing_dotfiles | while read -r x; do
+        if config cat-file -e "HEAD:$x" 2>/dev/null; then
+            config checkout HEAD -- "$x"
+        fi
+    done
 
     # Don't show untracked files, in order to ignore programs that dump their
     # configuration in $HOME.
